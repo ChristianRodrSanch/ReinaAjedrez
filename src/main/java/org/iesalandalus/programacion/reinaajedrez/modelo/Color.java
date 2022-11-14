e237e867077bd0f0e2a788639452f22423ed7146
pirul <pirul@192.168.11.7>
2022-11-14 14:23:40 +0100

    Creado atributo, y constructor cadenaAMostrar

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Color.java
@@ -3,4 +3,11 @@ package org.iesalandalus.programacion.reinaajedrez.modelo;
 public enum Color {
 Blanco,
 Negro;
+	private String cadenaAMostrar;
+
+	private Color(String cadenaAMostrar) {
+		this.cadenaAMostrar = cadenaAMostrar;
+	}
+	
+	
 }
