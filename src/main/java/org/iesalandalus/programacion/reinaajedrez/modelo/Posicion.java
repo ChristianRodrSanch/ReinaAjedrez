ffabbdb86708d1e476644a9afe48ae42a268aff3
pirul <pirul@Christian>
2022-11-15 18:56:59 +0100

    He creado los atributos get y set con su respectivo illegalArgumentException

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -1,6 +1,32 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
 public class Posicion {
-private int fila;
-private char columna;
+	private int fila;
+	private char columna;
+
+	public int getFila() {
+		return fila;
+	}
+
+	public void setFila(int fila) {
+		this.fila = fila;
+		if (fila < 1) {
+			throw new IllegalArgumentException("El valor de la fila es menor que el mínimo permitido.");
+		} else if (fila > 8) {
+			throw new IllegalArgumentException("El valor de la fila es mayor que el mínimo permitido.");
+		}
+	}
+
+public char getColumna() {
+	return columna;
+}
+
+public void setColumna(char columna) {
+	this.columna = columna;
+	if (columna < a) {
+		throw new IllegalArgumentException("El valor de la columna es menor que el mínimo permitido.");
+ } else if (columna > h) {
+		throw new IllegalArgumentException("El valor de la fila es mayor que el mínimo permitido.");
+ }
+}
 }
