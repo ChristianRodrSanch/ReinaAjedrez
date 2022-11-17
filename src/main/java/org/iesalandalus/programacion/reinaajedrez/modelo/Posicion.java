e9727b1f3548b94373feec2ac9a143fe18f8d93c
pirul <pirul@192.168.11.7>
2022-11-17 13:19:16 +0100

    He creado los metodo equals y hashCode

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Posicion.java
@@ -1,5 +1,7 @@
 package org.iesalandalus.programacion.reinaajedrez.modelo;
 
+import java.util.Objects;
+
 public class Posicion {
 	private int fila;
 	private char columna;
@@ -31,6 +33,23 @@ public class Posicion {
 
 	}
 
+	@Override
+	public int hashCode() {
+		return Objects.hash(columna, fila);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posicion other = (Posicion) obj;
+		return columna == other.columna && fila == other.fila;
+	}
+
 	public void setColumna(char columna) {
 		this.columna = columna;
 		if (columna < 'a') {
@@ -38,5 +57,10 @@ public class Posicion {
 		} else if (columna > 'h') {
 			throw new IllegalArgumentException("El valor de la fila es mayor que el mínimo permitido.");
 		}
+	}
+
+	public Posicion(int fila, char columna) {
+		setFila(fila);
+		setColumna(columna);
 	}
 }
