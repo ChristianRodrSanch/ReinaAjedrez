0c5f96e392bf78c45765e2af0bb87c8cbe9903fe
pirul <pirul@192.168.1.219>
2022-11-17 20:31:46 +0100

    He creado un static void de mostrar Menu, con sus syso de Crear reina eligiento el color,mover,salir

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -3,4 +3,10 @@ package org.iesalandalus.programacion.reinaajedrez;
 public class Consola {
 private Consola() {
 }
+public static void mostraMenu() {
+	System.out.println("1. Crear Reina por defecto");
+	System.out.println("2.Crear reina eligiendo el color");
+	System.out.println("3.Mover");
+	System.out.println("4.Salir");
+}
 }
