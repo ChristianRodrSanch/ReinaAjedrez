64008b95f25bd8388fa6c5b9c91b798bb3ea13c4
pirul <pirul@192.168.11.7>
2022-11-17 13:30:05 +0100

    He crado el getter y setter de posicion y color

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/modelo/Reina.java
@@ -3,4 +3,17 @@ package org.iesalandalus.programacion.reinaajedrez.modelo;
 public class Reina {
 private Color color;
 private Posicion posicion;
+public Color getColor() {
+	return color;
+}
+public void setColor(Color color) {
+	this.color = color;
+}
+public Posicion getPosicion() {
+	return posicion;
+}
+public void setPosicion(Posicion posicion) {
+	this.posicion = posicion;
+}
+
 }
