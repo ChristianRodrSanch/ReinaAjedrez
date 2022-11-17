bb78cbe369c750305e47a91baddd09b4074aded7
pirul <pirul@192.168.1.219>
2022-11-17 20:57:55 +0100

    He hecho la opcion Mostrar Menu con sus Syso y su elegirOpcionMenu con sus respectivo do,while y sus switch y case

--- src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
+++ src/main/java/org/iesalandalus/programacion/reinaajedrez/Consola.java
@@ -1,12 +1,40 @@
 package org.iesalandalus.programacion.reinaajedrez;
 
+import org.iesalandalus.programacion.utilidades.Entrada;
+
 public class Consola {
-private Consola() {
-}
-public static void mostraMenu() {
-	System.out.println("1. Crear Reina por defecto");
-	System.out.println("2.Crear reina eligiendo el color");
-	System.out.println("3.Mover");
-	System.out.println("4.Salir");
-}
+	private Consola() {
+	}
+
+	public static void mostraMenu() {
+		System.out.println("1. Crear Reina por defecto");
+		System.out.println("2.Crear reina eligiendo el color");
+		System.out.println("3.Mover");
+		System.out.println("4.Salir");
+	}
+
+public static int elegirOpcionMenu() {
+	int opcionMenu;
+ do {  
+	 System.out.println("elege una opcion del menú anterior");
+	 opcionMenu= Entrada.entero();
+ } while (opcionMenu <1 || opcionMenu >4);
+ System.out.println("La opcion elegida no es valida");
+ switch (opcionMenu) {
+ case 1:
+   System.out.println("Crear Reina por defecto");
+   break;
+ case 2:
+   System.out.println("Crear reina eleigiendo el color");
+   break;
+ case 3:
+   System.out.println("Mover");
+   break;
+ case 4:
+   System.out.println("Salir");
+   break;
+ }
+ 
+ }
+return opcionMenu;
 }
